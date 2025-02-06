2b05a0b0a46b756ff68dd5502549a2389bc61e68
pf-haidar <pedrohfilho.dev@gmail.com>
2025-02-06 19:48:06 -0400

    config: Bearer token on Expenses endpoint and Swagger

--- src/CashFlow.Api/obj/Debug/net8.0/CashFlow.Api.AssemblyInfo.cs
+++ src/CashFlow.Api/obj/Debug/net8.0/CashFlow.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CashFlow.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ce8de1bba99b82bdb6f9b8abcc166d881e0f0fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c767b4e0732808d65cc4700465fe1b811b5d004")]
 [assembly: System.Reflection.AssemblyProductAttribute("CashFlow.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CashFlow.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
